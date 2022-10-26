0944fdaee1160a475fc59f1fc7d147836d342c76
Facundo Romano <facundo-romano@hotmail.com>
2022-10-26 15:57:02 -0300

    CORS policy configured to localhost:3000/*

--- Sa_Turno_BackEnd/Program.cs
+++ Sa_Turno_BackEnd/Program.cs
@@ -2,6 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000",
+                                "*");
+        });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
---
+++ Sa_Turno_BackEnd/Startup.cs
@@ -0,0 +1,8 @@
+﻿namespace Sa_Turno_BackEnd
+{
+    public class Startup
+    {
+        //PASAR EL CONTENIDO DE PROGRAM A START-UP
+        // PROGRAM SOLO INICIA LA FUNCION DENTRO  DE STARTUP 
+    }
+}
