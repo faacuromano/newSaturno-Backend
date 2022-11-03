1aaf93e91fc6d754fdc09050c865be1792126337
Facundo Romano <facundo-romano@hotmail.com>
2022-11-02 22:02:01 -0300

    Initial Migrate to DB

---
+++ Sa_Turno_BackEnd/Data/Entitys/Turno.cs
@@ -0,0 +1,10 @@
+﻿namespace Sa_Turno_BackEnd.Entitys
+{
+    public class Turno
+    {
+        public int Id { get; set; }
+        public string Fecha { get; set; }
+        public string Horario { get; set; }
+        public string Hash { get; set; }
+    }
+}
